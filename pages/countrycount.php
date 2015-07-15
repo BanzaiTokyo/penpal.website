ebb44e09733ac433b8be250d96af04ad11f15ec8
BanzaiTokyo <banzaitokyo@gmail.com>
2015-07-15 18:27:57 +0200

    minor error in country count
    minor error in country count

--- pages/countrycount.php
+++ pages/countrycount.php
@@ -1,6 +1,7 @@
 <?php
 $request='select  count(distinct(c.id_country))
-from countries as c join members as m on c.id_country=m.country;';
+from countries as c join members as m on c.id_country=m.country
+where m.level>=3;';
 $res=mysql_query($request);
 echo mysql_result($res, 0);
 ?>
